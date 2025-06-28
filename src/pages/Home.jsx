5d57335dcb45ca510859e2f04d7d47af02e40272
Blackstate9080 <is7636665@gmail.com>
2025-06-28 22:37:33 +0530

    Booking button in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Background } from "../components/Background";
 import { Navbar } from "../components/Navbar";
 import { Home_Background } from "../components/Home/Home_Background";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
@@ -13,7 +14,22 @@ export const Home = () => {
         <div className="text-gray-300 font-light">to</div>
         <div className="text-white text-7xl font-cabin-sketch">The Hotel</div>
         </h1>
-        
+        <p className="text-xl text-center font-extralight text-white max-w-2xl mx-auto mb-6 bg-black/30 px-4 py-2 rounded-3xl">
+          Welcome to our premium hotel booking platform — where comfort meets luxury.
+          Whether you're planning a romantic getaway, a family vacation, or a business
+          trip, we have the perfect stay waiting for you.
+        </p>
+        <div className="text-center mt-4">
+        <Link
+          to="/booking"
+          className="mt-6 inline-block border border-white text-white px-8 py-3 rounded-full text-lg tracking-wide
+             font-light uppercase transition duration-300 ease-in-out hover:bg-white hover:text-black shadow-sm
+             hover:shadow-md"
+        >
+          Book Now
+        </Link>
+      </div>
+                
       </Home_Background>
     </Background>
   );
